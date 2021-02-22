db994c315192d6aa9544b2ad779c607b7ff0c8d8
Egor Chernakov <chernakov.eg@gmail.com>
2021-02-22 17:06:37 +0300

    Initial Commit

---
+++ WordScramble/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by Egor Chernakov on 22.02.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
